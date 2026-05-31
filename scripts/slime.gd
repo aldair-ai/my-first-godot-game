da3d1fdb4cef36a7b90be2c70a15f0ea7c8d21b1
Edwin Aldair Espinoza Zegarra <62805950+aldair-ai@users.noreply.github.com>
2026-05-31 00:00:53 -0700

    feat: enemy updated movement both directions

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
 const SPEED = 60
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var ray_cast_right: RayCast2D = $RayCastRight
 
+var direction = 1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,5 +12,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.x += SPEED * delta
+	if ray_cast_right.is_colliding():
+		direction = -1
+	
+	if ray_cast_left.is_colliding():
+		direction = 1
+	
+	position.x += direction * SPEED * delta
 	pass
